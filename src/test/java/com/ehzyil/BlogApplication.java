a996be6dee3babd36e78b745a879bce403fec2d2
ehZyiL <liyuan.0217@qq.com>
2023-09-30 22:40:29 +0800

    查看分类下的文章fix

---
+++ src/test/java/com/ehzyil/BlogApplication.java
@@ -0,0 +1,19 @@
+package com.ehzyil;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author ehyzil
+ * @Description
+ * @create 2023-09-2023/9/25-19:48
+ */
+
+//@MapperScan("com.ehzyil.mapper")
+@SpringBootTest
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class);
+    }
+}
